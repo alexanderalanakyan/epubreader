a2f70b31cea15909701b1f36ce9b5f8fc06e4b83
Alexander Alanakyan <73045206+alexanderalanakyan@users.noreply.github.com>
2026-04-20 22:48:55 +0000

    Adding compiler logic

---
+++ build/CMakeFiles/epubreader.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/epubreader.dir/src/main.cpp.o"
+  "CMakeFiles/epubreader.dir/src/main.cpp.o.d"
+  "prod/epubreader"
+  "prod/epubreader.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/epubreader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/epubreader.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for epubreader.
+# This may be replaced when dependencies are built.
